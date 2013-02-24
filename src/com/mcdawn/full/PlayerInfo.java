3ef31a0bec552fe69fcf30d028b0a2c61e8bba55
Jonny <jonny_li@live.ca>
2013-02-24 01:27:05 -0800

    PlayerInfo.isDev changed to non-static method

--- src/com/mcdawn/full/PlayerInfo.java
+++ src/com/mcdawn/full/PlayerInfo.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.Player;
 public class PlayerInfo {
 	public static List<String> getDevs() { return Arrays.asList("jonnyli1125", "incedo"); }
 	
-	public static boolean isDev(String username) { return getDevs().contains(username.toLowerCase()); }
+	public boolean isDev() { return getDevs().contains(thisPlayer.getName().toLowerCase()); }
 	
 	public Player thisPlayer;
 	
