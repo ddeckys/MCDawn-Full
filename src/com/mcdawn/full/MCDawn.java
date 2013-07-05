b49cac3991a934e393781e8efbbdd257644c864d
Dominic Deckys <ddeckys@tridomcraft.com>
2013-07-04 19:39:19 -0600

    Changed some stuffz

--- src/com/mcdawn/full/MCDawn.java
+++ src/com/mcdawn/full/MCDawn.java
@@ -1,9 +1,11 @@
 package com.mcdawn.full;
 
+import java.io.FileNotFoundException;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.*;
 
 import com.mcdawn.full.irc.*;
@@ -19,11 +21,15 @@ public class MCDawn extends JavaPlugin implements Listener {
 	public static IRC irc;
 	public static GlobalChat globalChat;
 	
+	/**
+	 * Used for saving information in a data file.
+	 */
+	public static FileConfiguration data = new YamlConfiguration();
+	
 	@Override
 	public void onEnable() {
 		thisPlugin = this;
 		logger = getLogger();
-		logger.info("Enabled MCDawn, v" + getVersion() + (getExtraVersion() == "" ? "" : "-" + getExtraVersion() + "."));
 		getServer().getPluginManager().registerEvents(new EventListener(), this);
 		Util.setupConfig();
 		Util.setupCommands();
@@ -31,12 +37,25 @@ public class MCDawn extends JavaPlugin implements Listener {
 		FileConfiguration config = getConfig();
 		if (config.getBoolean("irc.useIRC")) irc = new IRC();
 		if (config.getBoolean("globalchat.useGlobalChat")) globalChat = new GlobalChat();
+		try{
+			data.load("plugins/Ddeckys headnames/data.yml");
+		} catch (FileNotFoundException e) {
+			logger.info("Could not find the default data file, one will be created on restart.");
+		} catch (Exception e) {
+			logger.info(e.getMessage());
+		}
+		logger.info("Enabled MCDawn, v" + getVersion() + (getExtraVersion() == "" ? "" : "-" + getExtraVersion() + "."));
 	}
 	
 	@Override
 	public void onDisable() {
 		if (irc != null && irc.isConnected()) irc.shutdown();
 		if (globalChat != null && globalChat.isConnected()) globalChat.shutdown();
+		try {
+			data.save("plugins/Ddeckys headnames/data.yml");
+		} catch (Exception e) {
+			logger.info(e.getMessage());
+		}
 		logger.info("Disabled MCDawn.");
 	}
 }
--- src/com/mcdawn/full/PlayerInfo.java
+++ src/com/mcdawn/full/PlayerInfo.java
@@ -2,17 +2,37 @@ package com.mcdawn.full;
 
 import java.util.*;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import com.mcdawn.full.database.*;
 
 public class PlayerInfo {
-	public static List<String> getDevs() { return Arrays.asList("jonnyli1125", "incedo"); }
+	public static List<String> getDevs() { return Arrays.asList("jonnyli1125", "incedo", "ddeckys"); }
 	public static Table getTable() { return new Table("Players"); }
 	
-	public Player thisPlayer;
+	private Player player;
 	
-	public PlayerInfo(Player p) { thisPlayer = p; }
-	public boolean isDev() { return getDevs().contains(thisPlayer.getName().toLowerCase()); }
+	public PlayerInfo(Player p) {
+		player = p;
+	}
+	
+	public Player getPlayer () {
+		return player;
+	}
+	
+	public boolean isDev() {
+		return getDevs().contains(player.getName().toLowerCase());
+	}
+	
+	public static PlayerInfo[] getOnlinePlayers() {
+		PlayerInfo[] list;
+		Player[] plist = Bukkit.getOnlinePlayers();
+		list = new PlayerInfo[plist.length];
+		for (int t=0; t<plist.length; t++){
+			list[t]=new PlayerInfo(plist[t]);
+		}
+		return list;
+	}
 	
 }
